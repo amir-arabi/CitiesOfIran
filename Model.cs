26dd5666d5b29c2eb6af359e3d3f0c5cb83afa82
Amir Arabi <27159629+amir-arabi@users.noreply.github.com>
2020-02-27 15:44:41 +0330

    Add project files.

---
+++ Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CitiesOfIran
+{
+    class Model
+    {
+        public string State { get; set; }
+        public List<string> Cities { get; set; }
+        
+        public Model(string state, List<string> cities)
+        {
+            State = state ?? throw new ArgumentNullException(nameof(state));
+            Cities = cities ?? throw new ArgumentNullException(nameof(cities));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace CitiesOfIran
+{
+    class Program
+    {
+        private const string inputPath = @".\res\GEO97.xlsx";
+        private const string outputPath = @".\res\CitiesOfIran.json";
+        static void Main()
+        {
+            var keyValuePairs = new SortedDictionary<string, SortedSet<string>>();
+            using var excelPackage = new ExcelPackage(new FileInfo(inputPath));
+            var worksheet = excelPackage.Workbook.Worksheets["تقسیمات کشوری 1397"];
+            string state, city;
+            int count = 2;
+            for (int i = 0; i < 33; i++)
+            {
+                var citiesSortedSet = new SortedSet<string>();
+                do
+                {
+                    state = worksheet.Cells["A" + count].Text.Trim();
+                    city = worksheet.Cells["B" + count].Text.Trim();
+                    if (!string.IsNullOrEmpty(city))
+                    {
+                        citiesSortedSet.Add(city);
+                    }
+                    count++;
+                } while (state == worksheet.Cells["A" + count].Text.Trim());
+                if (!string.IsNullOrEmpty(state) && !keyValuePairs.ContainsKey(state))
+                {
+                    keyValuePairs.Add(state, citiesSortedSet);
+                }
+            }
+
+            var list = new List<Model>();
+            foreach (var (key, value) in keyValuePairs)
+            {
+                var l = new List<string>();
+                foreach (var item in value)
+                {
+                    l.Add(item);
+                }
+                list.Add(new Model(key, l));
+            }
+            //var jsonOptions = new JsonSerializerOptions()
+            //{
+            //    WriteIndented = true,
+            //    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            //};
+            //var json = JsonSerializer.SerializeToUtf8Bytes(list, list.GetType(), jsonOptions);
+            //File.WriteAllText(outputPath, Encoding.UTF8.GetString(json));
+            File.WriteAllText(outputPath, JsonUtf8Serializer(ref list));
+        }
+
+        public static string JsonUtf8Serializer<T>(ref T t)
+        {
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            };
+            byte[] json = JsonSerializer.SerializeToUtf8Bytes(t, t.GetType(), jsonOptions);
+            return Encoding.UTF8.GetString(json);
+        }
+    }
+}
